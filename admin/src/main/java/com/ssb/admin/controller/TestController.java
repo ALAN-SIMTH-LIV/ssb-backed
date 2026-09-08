ee39fca8486ea8d06b71c4a40c29bbee02ffd042
ALAN-SIMTH-LIV <146905929+ALAN-SIMTH-LIV@users.noreply.github.com>
2026-09-08 22:31:18 +0800

    初始化配置

---
+++ admin/src/main/java/com/ssb/admin/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.ssb.admin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/api")
+public class TestController {
+
+    @GetMapping(value = "/test")
+    private String test(){
+        return "这里是ADMIN端";
+    }
+}
---
+++ admin/src/test/java/com/ssb/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssb.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
