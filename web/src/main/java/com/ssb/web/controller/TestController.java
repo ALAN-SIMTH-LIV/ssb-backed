b5e673951f3db3ca7642c0c0c800d2954f4b4256
ALAN-SIMTH-LIV <146905929+ALAN-SIMTH-LIV@users.noreply.github.com>
2026-09-09 23:52:34 +0800

    修改了点错误代码，实现前端测试跨域访问

--- web/src/main/java/com/ssb/web/controller/TestController.java
+++ web/src/main/java/com/ssb/web/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.ssb.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/api")
+@RestController
+@RequestMapping(value = "/api")
 public class TestController {
 
     @GetMapping(value = "/test")
